4d155bb3e2193105a38fef1c6cd7ce4dd62ce915
Jose Moran Urena <jose@josemoranurena.tech>
2024-09-05 22:29:30 -0400

    fix bug

--- freqtrade/entrypoint.sh
+++ freqtrade/entrypoint.sh
@@ -2,14 +2,18 @@
 
 CONFIG_PATH="${APP_DATA_DIR}/user_data/config.json"
 
+if [ ! -d "${APP_DATA_DIR}/user_data" ]; then
+    echo "Creating user directory structure..."
+    freqtrade create-userdir --userdir user_data
+fi
+
 if [ ! -f "$CONFIG_PATH" ]; then
     echo "Config file not found. Creating default config.json..."
-    freqtrade create-userdir --userdir user_data
     freqtrade new-config --config "$CONFIG_PATH"
 else
     echo "Config file exists. Skipping creation."
 fi
 
-freqtrade trade --logfile "${APP_DATA_DIR}/user_data/logs/freqtrade.log" \
-                --db-url sqlite:///${APP_DATA_DIR}/user_data/tradesv3.sqlite \
-                --config "$CONFIG_PATH" --strategy SampleStrategy
+exec freqtrade trade --logfile "${APP_DATA_DIR}/user_data/logs/freqtrade.log" \
+                     --db-url sqlite:///${APP_DATA_DIR}/user_data/tradesv3.sqlite \
+                     --config "$CONFIG_PATH" --strategy SampleStrategy
